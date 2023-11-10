4b83e1423227a8f4bdee24a31500ce691deb8d71
Michael McMahon <michael@gnu.org>
2023-11-09 23:30:11 -0500

    Add echo commands

--- install.sh
+++ install.sh
@@ -1,11 +1,31 @@
-mkdir ~/builds
+#!/bin/bash
+
+set -euo pipefail
+
+# TODO Check if git is found.
+
+mkdir -p ~/builds
+mkdir -p ~/.config/i3
+
 cd ~/builds
+echo "Downloading TechnologyClassroom/dotfiles..."
 git clone http://github.com/technologyclassroom/dotfiles
 cd dotfiles/
 
-cp jokes.txt ~/
-cp nanorc /etc/nanorc
+echo "Installing i3 config..."
+cp i3 ~/.config/i3/config
+echo "Installing nano config..."
+sudo cp nanorc /etc/nanorc
+echo "Installing tmux config..."
 cp tmux.conf ~/.tmux.conf
+echo "Installing vim config..."
 cp vimrc ~/.vimrc
-#cat bashrc >> /etc/bash.bashrc
-#exec bash
+#echo "Installing BASH config..."
+#cat bashrc >> ~/.bashrc
+#source ~/.bashrc
+
+cd ~/builds
+echo "Downloading TechnologyClassroom/hitme..."
+git clone https://github.com/TechnologyClassroom/hitme
+echo "Installing jokes.txt..."
+cp hitme/jokes.txt ~/
